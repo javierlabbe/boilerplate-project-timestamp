5c2d718b809b428606896469531ea1201ae9c186
Javier Labbé <javier.labbe@uc.cl>
2023-09-12 16:03:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -20,13 +20,13 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+const routerDate = require(__dirname+'/routers/date.js')
 
-
+app.use('/', routerDate);
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+const PORT = process.env.PORT;
+
+var listener = app.listen(PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
